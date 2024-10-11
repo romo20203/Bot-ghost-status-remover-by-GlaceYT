4b6129c356476d8b7af9b8bd164b29f97ce6ed46
romo20203 <hfjddghrdf@gmail.com>
2024-10-11 18:45:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ function updateStatus() {
   const currentStatus = statusMessages[currentStatusIndex];
   const currentType = statusTypes[currentTypeIndex];
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom }],
-    status: "dnd",
+    activities: [{ name: `${totalMembers} `,, type: ActivityType.Watching, }],
+    status: "online",
   });
   console.log('\x1b[33m[ STATUS ]\x1b[0m', `Updated status to: ${currentStatus} (${currentType})`);
   currentStatusIndex = (currentStatusIndex + 1) % statusMessages.length;
